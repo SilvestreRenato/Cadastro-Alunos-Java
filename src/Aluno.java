a027e21c47094fe61cfc195d6c099cabd6ce47a7
Renato <silvestrerenato17@gmail.com>
2025-10-07 19:59:51 -0400

    Creating class Aluno

---
+++ src/Aluno.java
@@ -0,0 +1,44 @@
+class Aluno {
+        private String nome;
+        private String matricula;
+        private double nota1;
+        private double nota2;
+
+        public Aluno (String nome, String matricula, double nota1, double nota2) {
+            this.nome = nome;
+            this.matricula = matricula;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+
+        public double calcularMedia() {
+            double media = (getNota1() + getNota2()) / 2;
+            return media;
+        }
+
+        public void exibirInformacoes() {
+            System.out.println(
+                    "Aluno: " + getNome() + ".\n" +
+                            "Matricula: " + getMatricula() + ".\n" +
+                            "Nota 1: " + getNota1() + ".\n" +
+                            "Nota 2: " + getNota2() + ".\n" +
+                            "Média do aluno: " + calcularMedia()
+            );
+        }
+
+        public String getNome() {
+            return this.nome;
+        }
+
+        public String getMatricula() {
+            return this.matricula;
+        }
+
+        public double getNota1() {
+            return this.nota1;
+        }
+
+        public double getNota2() {
+            return this.nota2;
+        }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+        Aluno a1 = new Aluno("Joao Silva", "A123", 8.5, 7.0);
+        Aluno a2 = new Aluno("Maria Souza", "B723", 9.0, 9.5);
+        a1.exibirInformacoes();
+        System.out.println();
+        a2.exibirInformacoes();
     }
 }
